068ae7f19ed3b986b138b4bcb3fe4750f259b10f
anushakanda <71995197+anushakanda@users.noreply.github.com>
2021-02-12 14:51:51 +0530

    Create ImageFileEditor.java
    Service Interface

---
+++ dependencyinjection/CDI/services/imagefileeditors/ImageFileEditor.java
@@ -0,0 +1,10 @@
+public interface ImageFileEditor {
+    
+    String openFile(String fileName);
+    
+    String editFile(String fileName);
+    
+    String writeFile(String fileName);
+    
+    String saveFile(String fileName);
+}
